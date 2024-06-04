b9555b50da3f93d66df251123fb47e44ada3fe45
junior stratznovich <juniorsouzaprod@gmail.com>
2024-06-04 16:06:35 -0300

    desafios de logica bootcamp Dio

---
+++ DesafioControleFluxo/src/com/VerificadorNumeroConta.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.util.Scanner;
+
+public class VerificadorNumeroConta {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String numeroConta = scanner.nextLine();
+            verificarNumeroConta(numeroConta);
+
+            // TODO: Chamar o método que veritica se o número da conta é valido
+            
+            // Caso nenhuma exceção seja lançada, imprime a mensagem de sucesso.
+            System.out.println("Numero de conta valido.");
+        } catch (IllegalArgumentException e) {
+            // TODO: Informar que o número de conta é inválido e exibir a mensagem de erro
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            // Fechar o scanner para evitar vazamentos de recursos
+            scanner.close();
+        }
+    }
+
+    private static void verificarNumeroConta(String numeroConta) {
+        if (numeroConta.length() != 8) {
+        	throw new IllegalArgumentException("Numero de conta invalido. Digite exatamente 8 digitos.");
+        	
+            // TODO: Lançar uma IllegalArgumentException com a seguinte mensagem:
+            // "Numero de conta invalido. Digite exatamente 8 digitos."
+        }
+    }
+}
